97c3412c516f980e94e711d2039bce3a480aa7dc
Johan Vos <johan@lodgon.com>
2019-09-21 22:01:20 +0200

    add more CQC implementation

--- src/main/java/com/gluonhq/strange/cqc/CQCSession.java
+++ src/main/java/com/gluonhq/strange/cqc/CQCSession.java
@@ -1,20 +1,27 @@
 package com.gluonhq.strange.cqc;
 
+import com.gluonhq.strange.Gate;
+import com.gluonhq.strange.gate.*;
+
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-
 public class CQCSession {
 
     private Socket socket;
     private OutputStream os;
     private InputStream is;
-    private short appId;
+    private final short appId;
 
     public CQCSession() {
         appId = getNextAppId();
+    }
+
+    public CQCSession(short id) {
+        appId = id;
     }
 
     public void connect(String host, int port) throws IOException {
@@ -29,28 +36,58 @@ public class CQCSession {
         sendCqcHeader(Protocol.CQC_TP_HELLO, 0);
     }
 
-    public void createQubit() throws IOException {
-        sendSimpleCommand(Protocol.CQC_CMD_NEW, (short)0);
+    public int createQubit() throws IOException {
+        sendCqcHeader(Protocol.CQC_TP_COMMAND, 4);
+        sendCommandHeader((short) 0, Protocol.CQC_CMD_NEW, (byte) 0);
+        ResponseMessage msg = readMessage();
+        return msg.getQubitId();
     }
 
-    private void sendComman() {
-
+    public int createEPR(String name, short port) throws IOException {
+        sendCqcHeader(Protocol.CQC_TP_COMMAND, 12);
+        sendCommandHeader((short) 0, Protocol.CQC_CMD_EPR, (byte) 0);
+        sendCommunicationHeader((short)0, port, 127*256*256*256+1);
+        ResponseMessage msg = readMessage();
+        return msg.getQubitId();
     }
-    public void createEPR(String name) throws IOException {
-        sendCommand(Protocol.CQC_CMD_EPR,(short)0,true,false, true, 12);
-   //     sendSimpleCommand(Protocol.CQC_CMD_EPR, (short)0);
+
+    public void applyGate(Gate gate) throws IOException {
+        sendCqcHeader(Protocol.CQC_TP_COMMAND, 4);
+        int qid = gate.getMainQubitIndex();
+        byte cmdByte = 0;
+        if (gate instanceof X) {
+            cmdByte = Protocol.CQC_CMD_X;
+        } else if (gate instanceof Hadamard) {
+            cmdByte = Protocol.CQC_CMD_H;
+        } else if (gate instanceof Cnot) {
+            cmdByte = Protocol.CQC_CMD_CNOT;
+        }
+        System.err.println("Send command to apply gate");
+        sendCommandHeader((short) qid, cmdByte, (byte) 0);
+        System.err.println("Sent command done, read reply");
+        ResponseMessage msg = readMessage();
+        System.err.println("reply done");
+
+
     }
 
     public ResponseMessage readMessage() throws IOException {
         if (is == null) {
             is = socket.getInputStream();
         }
-        byte[] msg = new byte[256];
-        int len = is.read(msg);
-        if (len < 8) {
-            throw new IOException ("Can't read message if it has less than 8 bytes (got "+len+")");
-        }
-        ResponseMessage answer = new ResponseMessage(msg, len);
+        ResponseMessage answer = new ResponseMessage(is);
+//        byte[] msg = new byte[256];
+//        DataInputStream dis = new DataInputStream(is);
+//        byte protocol = dis.readByte();
+//        int len = is.read(msg);
+//        for (int i = 0; i < len; i ++) {
+//            System.err.println("b["+i+"] = "+msg[i]);
+//        }
+//        System.err.println("Response from CQC has length "+len);
+//        if (len < 8) {
+//            throw new IOException ("Can't read message if it has less than 8 bytes (got "+len+")");
+//        }
+//        ResponseMessage answer = new ResponseMessage(msg, len);
         return answer;
     }
 
@@ -74,18 +111,25 @@ public class CQCSession {
         dos.flush();
     }
 
+    private void sendCommandHeader(short qubit_id, byte command, byte option) throws IOException {
+        DataOutputStream dos = new DataOutputStream(os);
+        dos.writeShort(qubit_id); // qubit_id
+        dos.writeByte(command);
+        dos.writeByte(option);
+        dos.flush();
+    }
+
+    private void sendCommunicationHeader(short appId, short port, int host) throws IOException {
+        DataOutputStream dos = new DataOutputStream(os);
+        dos.writeShort(appId);
+        dos.writeShort(port);
+        dos.writeInt(host);
+        dos.flush();
+    }
+
     private void sendCommand(byte command, short qubit_id, boolean notify, boolean action, boolean block, int length) throws IOException {
         sendCqcHeader(Protocol.CQC_TP_COMMAND, length);
-        if (command == Protocol.CQC_CMD_EPR) {
-            DataOutputStream dos = new DataOutputStream(os);
-            dos.writeShort(0); // qubit_id
-            dos.writeByte(command);
-            dos.writeByte(0);
-dos.writeShort(1);
-dos.writeShort(2);
-dos.writeInt(4);
-dos.flush();
-        }
+
 
     }
 
--- src/main/java/com/gluonhq/strange/cqc/Protocol.java
+++ src/main/java/com/gluonhq/strange/cqc/Protocol.java
@@ -6,10 +6,14 @@ public class Protocol {
 
     public static final byte CQC_TP_HELLO = 0x0;
     public static final byte CQC_TP_COMMAND = 0x1;
-    public static final byte CQC_TP_EPR_OK =     0x6;
+    public static final byte CQC_TP_EPR_OK = 0x6;
+    public static final byte CQC_TP_NEW_OK = 0xA;
 
     public static final byte CQC_CMD_NEW = 0x1;
     public static final byte CQC_CMD_EPR = 0x7;
     public static final byte CQC_CMD_EPR_RECV = 0x8;
+    public static final byte CQC_CMD_X = 0xA;
+    public static final byte CQC_CMD_H = 0x11;
+    public static final byte CQC_CMD_CNOT = 0x13;
 
 }
