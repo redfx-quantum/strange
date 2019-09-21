5509ee396e67384837a900fa188490c45929bbf7
Johan Vos <johan@lodgon.com>
2019-09-21 22:01:20 +0200

    add more CQC implementation

--- src/main/java/com/gluonhq/strange/cqc/ResponseMessage.java
+++ src/main/java/com/gluonhq/strange/cqc/ResponseMessage.java
@@ -1,8 +1,18 @@
 package com.gluonhq.strange.cqc;
 
+import java.io.DataInputStream;
+import java.io.InputStream;
+
 public class ResponseMessage {
 
-    final byte type;
+    private InputStream is;
+    byte type;
+    private short qubitId;
+
+    public ResponseMessage(InputStream is) {
+        this.is = is;
+        parseInputStream();
+    }
 
     public ResponseMessage(byte[] msg, int length) {
         type = msg[1];
@@ -10,5 +20,32 @@ public class ResponseMessage {
 
     public byte getType() {
         return type;
+    }
+
+    public short getQubitId() {
+        return this.qubitId;
+    }
+
+    private void parseInputStream() {
+        try {
+            DataInputStream dis = new DataInputStream(is);
+            byte protocol = dis.readByte();
+            byte cmd = dis.readByte();
+            System.err.println("CMD = "+cmd);
+            short appId = dis.readShort();
+            int len = dis.readInt();
+            System.err.println("len = "+len);
+            if (cmd == Protocol.CQC_TP_NEW_OK) {
+                short qid = dis.readShort();
+                System.err.println("qid = "+qid);
+                this.qubitId = qid;
+            } else {
+                System.err.println("payload len = "+len);
+                byte[] payload = new byte[len];
+                dis.read(payload);
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
 }
