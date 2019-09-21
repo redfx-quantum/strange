4377a3d18e23e3e4fa6f38c691c943ad0675bfa5
Johan Vos <johan@lodgon.com>
2019-09-21 16:33:28 +0200

    more parts of CQC

--- src/main/java/com/gluonhq/strange/cqc/Protocol.java
+++ src/main/java/com/gluonhq/strange/cqc/Protocol.java
@@ -6,6 +6,10 @@ public class Protocol {
 
     public static final byte CQC_TP_HELLO = 0x0;
     public static final byte CQC_TP_COMMAND = 0x1;
+    public static final byte CQC_TP_EPR_OK =     0x6;
 
     public static final byte CQC_CMD_NEW = 0x1;
+    public static final byte CQC_CMD_EPR = 0x7;
+    public static final byte CQC_CMD_EPR_RECV = 0x8;
+
 }
