3a8ab39eec32d8ce49f5327218411d9191a5297b
Johan Vos <johan@lodgon.com>
2019-09-22 22:49:33 +0200

    Allow to teleport qubits from Java node to Python node

--- src/main/java/com/gluonhq/strange/cqc/Protocol.java
+++ src/main/java/com/gluonhq/strange/cqc/Protocol.java
@@ -6,14 +6,23 @@ public class Protocol {
 
     public static final byte CQC_TP_HELLO = 0x0;
     public static final byte CQC_TP_COMMAND = 0x1;
+    public static final byte CQC_TP_DONE = 0x4;
     public static final byte CQC_TP_EPR_OK = 0x6;
+    public static final byte CQC_TP_MEASOUT = 0x7;
     public static final byte CQC_TP_NEW_OK = 0xA;
 
     public static final byte CQC_CMD_NEW = 0x1;
+    public static final byte CQC_CMD_MEASURE = 0x2;
     public static final byte CQC_CMD_EPR = 0x7;
     public static final byte CQC_CMD_EPR_RECV = 0x8;
     public static final byte CQC_CMD_X = 0xA;
     public static final byte CQC_CMD_H = 0x11;
-    public static final byte CQC_CMD_CNOT = 0x13;
+    public static final byte CQC_CMD_CNOT = 0x14;
+
+    // OPTIONS
+    public static final byte CQC_OPT_NOTIFY = 0x1;
+    public static final byte CQC_OPT_ACTION = 0x2;
+    public static final byte CQC_OPT_BLOCK = 0x4;
+
 
 }
