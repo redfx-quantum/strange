10ab48d3bee671680493551013c73963739a3098
Johan Vos <johan@lodgon.com>
2020-01-26 13:50:44 +0100

    more codacy fixes

--- src/test/java/com/gluonhq/strange/test/OracleTests.java
+++ src/test/java/com/gluonhq/strange/test/OracleTests.java
@@ -39,7 +39,9 @@ import com.gluonhq.strange.Step;
 import com.gluonhq.strange.gate.Oracle;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 public class OracleTests extends BaseGateTests {
 
