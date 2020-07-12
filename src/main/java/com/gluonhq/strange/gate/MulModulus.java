2d25c3057cf432c7449031b2598ec904cf8bffdb
Johan Vos <johan@lodgon.com>
2020-07-12 20:52:43 +0200

    fix demo with expmodmul

--- src/main/java/com/gluonhq/strange/gate/MulModulus.java
+++ src/main/java/com/gluonhq/strange/gate/MulModulus.java
@@ -37,6 +37,7 @@ import com.gluonhq.strange.Complex;
 import com.gluonhq.strange.Gate;
 import com.gluonhq.strange.Step;
 import com.gluonhq.strange.local.Computations;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -45,8 +46,9 @@ import java.util.List;
  */
 public class MulModulus extends BlockGate<MulModulus> {
 
-    final Block block;
-    
+    Block block;
+    static HashMap<Integer, Block> cache = new HashMap<>();
+
     /**
      * Multiply the qubit in the x register with an integer mul
      * @param x0 start idx x register
@@ -66,33 +68,37 @@ public class MulModulus extends BlockGate<MulModulus> {
     }
     
     public Block createBlock(int y0, int y1, int mul, int mod) {
+         int hash = 1000000 * y0 + 10000*y1+ 100*mul + mod;
+        System.err.println("hash for "+y0+", " + y1+", ");
+        this.block = cache.get(hash);
+        if (block != null) return block;
         int x0 = 0;
         int x1 = y1-y0;
         int size = 1 + x1-x0;
      //   int dim = 1 << size;
         Block answer = new Block("MulModulus", 2 * size+1);
-        System.err.println("first blocks, add with "+x0+", "+x1+", size = "+size);
+    //    System.err.println("first blocks, add with "+x0+", "+x1+", size = "+size);
 
         for (int i = 0; i < mul; i++) {
-            System.err.println("CREATE ADD with "+x0+", "+x1+", "+(x1+1)+", "+(x1+size));
+     //       System.err.println("CREATE ADD with "+x0+", "+x1+", "+(x1+1)+", "+(x1+size));
             AddModulus add = new AddModulus(x0, x1, x1+1, x1 + size, mod);
             answer.addStep(new Step(add));
         }
 
         for (int i = x0; i < x1+1; i++) {
-            System.err.println("swap "+i+" with "+(i+size));
+      //      System.err.println("swap "+i+" with "+(i+size));
             answer.addStep(new Step (new Swap(i, i + size)));
         }
 
         int invsteps = Computations.getInverseModulus(mul,mod);
-        System.err.println("invsteps = "+invsteps);
+    //    System.err.println("invsteps = "+invsteps);
         for (int i = 0; i < invsteps; i++) {
             AddModulus add = new AddModulus(x0, x1, x1+1, x1 + size, mod).inverse();
-            System.err.println("CREATE ADDmod with "+x0+", "+x1+", "+(x1+1)+", "+(x1+size));
+      //      System.err.println("CREATE ADDmod with "+x0+", "+x1+", "+(x1+1)+", "+(x1+size));
 
             answer.addStep(new Step(add));
         }
-
+cache.put(hash, answer);
         return answer;
     }
   
