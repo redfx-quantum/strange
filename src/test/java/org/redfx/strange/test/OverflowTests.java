267a7a8b6e9596b51c31e2f56adb72215a154225
Johan Vos <johan@lodgon.com>
2021-06-06 11:29:58 +0200

    Test overflow of addition operation

---
+++ src/test/java/org/redfx/strange/test/OverflowTests.java
@@ -0,0 +1,112 @@
+/*-
+ * #%L
+ * Strange
+ * %%
+ * Copyright (C) 2021 Johan Vos
+ * %%
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the Johan Vos nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package org.redfx.strange.test;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.redfx.strange.Complex;
+import org.redfx.strange.Program;
+import org.redfx.strange.Qubit;
+import org.redfx.strange.Result;
+import org.redfx.strange.Step;
+import org.redfx.strange.gate.Add;
+import org.redfx.strange.gate.X;
+
+
+/**
+ * Tests on the size of blockgates
+ * @author johan
+ */
+public class OverflowTests extends BaseGateTests {
+
+    static final double D = 0.000000001d;
+    
+    @Test
+    public void add4() {
+        Program p = new Program(4);
+        Step prep = new Step();
+        p.addStep(prep);
+        Add add = new Add(0,1,2,3);
+        p.addStep(new Step(add));
+        Result result = runProgram(p);
+        Complex[] probs = result.getProbability();
+        Qubit[] q = result.getQubits();
+    }
+
+    @Test
+    public void addNotEnoughSpace() {
+        Program p = new Program(4);
+        Step prep = new Step();
+        p.addStep(prep);
+        Add add = new Add(1, 2, 3, 4);
+        p.addStep(new Step(add));
+        boolean iae = false;
+        try {
+            Result result = runProgram(p);
+        } catch (IllegalArgumentException e) {
+            iae = true;
+        }
+        assertTrue(iae);
+    }
+
+    // test to make sure an add gate does not influence nearby qubits
+    @Test
+    public void addNoOverlap() {
+        Program p = new Program(6);
+        Step prep = new Step(new X(2), new X(4));
+        p.addStep(prep);
+        Add add = new Add(1, 2, 3, 4);
+        p.addStep(new Step(add));
+        Result result = runProgram(p);
+        Complex[] probs = result.getProbability();
+        Qubit[] q = result.getQubits();
+        assertEquals(0, q[0].measure());
+        assertEquals(0, q[5].measure());
+    }
+    
+    // test to make sure an add gate does not influence nearby qubits
+    @Test
+    public void addNoOverlap2() {
+        Program p = new Program(6);
+        Step prep = new Step(new X(0), new X(2), new X(4), new X(5));
+        p.addStep(prep);
+        Add add = new Add(1, 2, 3, 4);
+        p.addStep(new Step(add));
+        Result result = runProgram(p);
+        Complex[] probs = result.getProbability();
+        Qubit[] q = result.getQubits();
+        assertEquals(1, q[0].measure());
+        assertEquals(1, q[5].measure());
+    }
+}
