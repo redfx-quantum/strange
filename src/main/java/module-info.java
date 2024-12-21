ac3689bbec4967f9a26a7bfd34cdc5520e5c0fb6
Johan Vos <johan@lodgon.com>
2024-12-21 17:00:46 +0100

    Add module-info.java (#143)
    Fix #142

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.redfx.strange {
+    requires java.logging;
+
+    exports org.redfx.strange;
+    exports org.redfx.strange.algorithm;
+    exports org.redfx.strange.gate;
+    exports org.redfx.strange.local;
+}
